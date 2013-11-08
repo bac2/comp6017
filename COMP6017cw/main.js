1788e0be38aa8302ab67558295d58ea1a6df537b
Ben Clive <bac2g10@ecs.soton.ac.uk>
2013-11-08 21:38:28 +0000

    Added post method for /question

--- COMP6017cw/main.js
+++ COMP6017cw/main.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(orm.express("sqlite://../database/sqlite.db", {
     define: function (db, models, next) {
         db.load("./models.js", function (err) {
-            if (err !== null) {
+            if (typeof err !== 'undefined') {
                 console.error(err);
             }
             models.question = db.models.question;
@@ -24,17 +24,20 @@ app.use(express.json());
 app.listen(1337, "0.0.0.0");
 
 app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello from " + req.url);
+    res.setHeader('Content-Type', 'application/json');
+    var obj = {'links': {'question':'/question'}};
+    res.end(JSON.stringify(obj) + "\n");
 });
 
+
+// /question methods
 app.get('/question', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     req.models.question.find([ "vote", "Z" ], function (err, questions) {
         // SQL: SELECT q.id, q.question, q.vote FROM Question q ORDER BY q.vote DESC
         var q,
             array = [];
-        if (err !== null) {
+        if (typeof err !== 'undefined') {
             console.error(err);
         }
         for (q = 0; q < questions.length; q = q + 1) {
@@ -46,12 +49,33 @@ app.get('/question', function (req, res) {
     });
 });
 
+app.post("/question", function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	req.models.question.create([
+	    {
+	        question: req.param("question"),
+	        vote: 0
+	    }
+    ], function (err, items) {
+		var i;
+        if (typeof err !== 'undefined') {
+            console.error(err);
+        }
+        for( i = 0; i < items.length; i = i + 1) {
+        	var item = items[i];
+        	res.status(201);
+        	res.setHeader("Location", "/question/"+ item['id']);
+        	res.end();
+        }
+	});
+});
+
 app.get('/question/:qid', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     req.models.question.find({ id : req.params.qid }, function (err, questions) {
         // SQL: SELECT q.id, q.question, q.vote FROM Question q WHERE q.id = id
         var q;
-        if (err !== null) {
+        if (typeof err !== 'undefined') {
             console.error(err);
         }
         for (q = 0; q < questions.length; q = q + 1) {
@@ -63,12 +87,12 @@ app.get('/question/:qid', function (req, res) {
 });
 
 app.get('/question/:qid/answer', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     req.models.answer.find({ question_id : req.params.qid }, [ "vote", "Z" ], function (err, answers) {
         //SQL: SELECT a.id, a.question_id, a.answer, a.vote FROM Answer a WHERE a.question_id = qid
         var a,
             array = [];
-        if (err !== null) {
+        if (typeof err !== 'undefined') {
             console.error(err);
         }
         for (a = 0; a < answers.length; a = a + 1) {
@@ -81,11 +105,11 @@ app.get('/question/:qid/answer', function (req, res) {
 });
 
 app.get('/question/:qid/answer/:aid', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     req.models.answer.find({ id : req.params.aid, question_id : req.params.qid }, function (err, answers) {
         //SQL: SELECT a.id, a.question_id, a.answer, a.vote FROM Answer a WHERE a.question_id = qid AND a.id = aid
         var a;
-        if (err !== null) {
+        if (typeof err !== 'undefined') {
             console.error(err);
         }
         for (a = 0; a < answers.length; a = a + 1) {
