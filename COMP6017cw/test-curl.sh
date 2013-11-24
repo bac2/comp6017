b93d67f9f61a62a63804b35d81a065a9bec4d473
Ben Clive <bac2g10@ecs.soton.ac.uk>
2013-11-24 16:55:59 +0000

    Fixed curl script. Fixed answer.js (miscopy)

--- COMP6017cw/test-curl.sh
+++ COMP6017cw/test-curl.sh
@@ -1,44 +1,44 @@
 # Get all questions
 echo "Get all questions:"
-curl -D- -X GET http://localhost:1337/question/ --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/ --max-time 2
 
 # Delete all questions
 echo "Delete all questions:"
-curl -D- -X DELETE http://localhost:1337/question/ --connect-timeout 1
+curl -D- -X DELETE http://localhost:1337/question/  --max-time 2
 
 # Add a new question
 echo "Add a new question:"
-curl -D- -X POST http://localhost:1337/question/ -H "Content-Type: application/json" -d '{"question":{"title":"How many cats in Japan?", "body":"Please help"}}' --connect-timeout 1
+curl -D- -X POST http://localhost:1337/question/ -H "Content-Type: application/json" -d '{"question":{"title":"How many cats in Japan?", "body":"Please help"}}' --max-time 1
 
 # Get a specific question
 echo "Get a specific question:"
-curl -D- -X GET http://localhost:1337/question/1  --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/1  --max-time 1
 
 # Get the answers to a question
 echo "Get answers to a question:"
-curl -D- -X GET http://localhost:1337/question/1/answer --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/1/answer --max-time 1
 
 # Get the comments on a question
 echo "Get the comments on a question:"
-curl -D- -X GET http://localhost:1337/question/1/comment --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/1/comment --max-time 1
 
 # Get a specific answer to a question
 echo "Get a specific answer to a question:"
-curl -D- -X GET http://localhost:1337/question/1/answer/0 --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/1/answer/0 --max-time 1
 
 # Get a specific comment on a question
 echo "Get a specific comment on a question:"
-curl -D- -X GET http://localhost:1337/question/1/answer/0/comment --connect-timeout 1
+curl -D- -X GET http://localhost:1337/question/1/answer/0/comment --max-time 1
 
 # Add a new answer to a question
 echo "Add a new answer to a question:"
-curl -D- -X POST http://localhost:1337/question/1/answer -H "Content-Type: application/json" -d '{"answer":{"body":"Several hundred"}}' --connect-timeout 1
+curl -D- -X POST http://localhost:1337/question/1/answer -H "Content-Type: application/json" -d '{"answer":{"body":"Several hundred"}}' --max-time 1
 
 # Add a new comment to a question
 echo "Add a new comment to a question:"
-curl -D- -X POST http://localhost:1337/question/1/comment -H "Content-Type: application/json" -d '{"comment":{"body":"Good question..."}}' --connect-timeout 1
+curl -D- -X POST http://localhost:1337/question/1/comment -H "Content-Type: application/json" -d '{"comment":{"body":"Good question..."}}' --max-time 1
 
 # Add a new comment on an answer
 echo "Add a new comment to an answer:"
-curl -D- -X POST http://localhost:1337/question/1/answer/0/comment -H "Content-Type: application/json" -d '{"comment":{"body":"Good answer..."}}' --connect-timeout 1
+curl -D- -X POST http://localhost:1337/question/1/answer/0/comment -H "Content-Type: application/json" -d '{"comment":{"body":"Good answer..."}}' --max-time 1
 
