dc8f8dc05b2e3d1574ff9f149f5afb06df70b718
Tom Thorpe <temt91@gmail.com>
2013-12-15 18:46:34 +0000

    answer post

--- COMP6017cw/answer_comment.js
+++ COMP6017cw/answer_comment.js
@@ -10,7 +10,53 @@ var root = function (req, res) {
         },
         
     	post: function (req, res) {
-    		
+    		res.setHeader('Content-Type', 'application/json');
+    		var comment = req.body['comment'];
+    		req.models.answer_comment.create([
+			    {
+			        answer_id: req.params.aid,
+			        comment: comment.comment,
+			        last_modified: new Date(),
+			        
+			    }
+		    ], function (err, items) {
+				var i;
+		        if (err) {
+		        	res.status(400);
+		        	res.end();
+		            console.error(err);
+		        }
+		        for( i = 0; i < items.length; i = i + 1) {
+		        	var item = items[i];
+		        	res.status(201);
+		        	res.setHeader("Location", "/question/" + req.params.qid + "/answer/" + req.params.aid + "/comment/" + item['id']);
+		        	res.end();
+		        }
+			});   
+        },
+        
+    	post: function (req, res) {
+    		req.models.question_comment.create([
+    		    {
+    		        question_id: req.params.qid,
+    		        comment: comment.comment,
+    		        last_modified: new Date(),
+    		        
+    		    }
+    	    ], function (err, items) {
+    			var i;
+    	        if (err) {
+    	        	res.status(400);
+    	        	res.end();
+    	            console.error(err);
+    	        }
+    	        for( i = 0; i < items.length; i = i + 1) {
+    	        	var item = items[i];
+    	        	res.status(201);
+    	        	res.setHeader("Location", "/question/" + req.params.qid + "/comment/" + item['id']);
+    	        	res.end();
+    	        }
+    		});   
         },
         
 		'delete': function (req, res) {
